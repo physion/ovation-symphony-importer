4412b8a0db909bd040e90761b662061c392e85a8
Barry Wark <barrywark@gmail.com>
2013-07-16 16:47:05 -0400

    Update to latest API

--- test/TestSymphonyImport.m
+++ test/TestSymphonyImport.m
@@ -1,8 +1,8 @@
-classdef TestSymphonyImport < OvationTestCase
+classdef TestSymphonyImport < MatlabTestCase
     methods
         
         function self = TestSymphonyImport(name)
-            self = self@TestSymphonySuite(name);
+            self = self@MatlabTestCase(name);
         end
         
         function testIntegration(self)
@@ -16,6 +16,9 @@ classdef TestSymphonyImport < OvationTestCase
             h5file = '110311Dc1.h5';
             metadata_xml = '110311Dc1_metadata.xml'; 
             
+            sourceProtocol = 'source-protocol';
+            context.insertProtocol(sourceProtocol, '<protocol doc>');
+            
             %% project and experiment
             for i=0:1
                 experimentPurpose='Test the Symphony import script works correctly';
@@ -26,19 +29,25 @@ classdef TestSymphonyImport < OvationTestCase
                 groups = SymphonyImport(self.context,...
                     fullfile(pwd(), pathToData, h5file),...
                     fullfile(pwd(), pathToData, metadata_xml),...
-                    exp);
+                    exp,...
+                    sourceProtocol);
                                                 
                 % Should have inserted one root EpochGroup
                 assert(length(groups) == 2);
                 
-                itr = exp.getEpochsIterable.iterator();
+                itr = exp.getEpochGroups().iterator();
                 n = 0;
                 while(itr.hasNext())
-                    n = n+1;
-                    itr.next();
+                    epochGroup = itr.next();
+                    eItr = epochGroup.getEpochs().iterator();
+                    while(eItr.hasNext())
+                        n = n+1;
+                        eItr.next();
+                    end
                 end
                 
                 % Should have inserted 20 epochs
+                disp([num2str(n) ' Epochs imported']);
                 assert(n == 20);
             end
         end
