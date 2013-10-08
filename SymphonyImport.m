48617fcf43ff086f4d3a7aae1cde38f74ac35b19
Barry Wark <barrywark@gmail.com>
2013-10-07 22:11:29 -0400

    Remove onCleanup
    Java call appears to let matlab elements go out of scope, triggering
    onCleanup and preventing data import.

--- SymphonyImport.m
+++ SymphonyImport.m
@@ -520,8 +520,6 @@ function readResponse(epoch, source, deviceName, deviceManufacturer, reader, res
     % Get dataspace and allocate memory for read buffer.
     %
     space = H5D.get_space (dset);
-    [~, dims, ~] = H5S.get_simple_extent_dims (space);
-    dims = fliplr(dims);
     
     %
     % Read the data.
@@ -529,7 +527,7 @@ function readResponse(epoch, source, deviceName, deviceManufacturer, reader, res
     datatype = H5T.open(file, 'MEASUREMENT');
     rdata=H5D.read (dset, datatype, 'H5S_ALL', 'H5S_ALL', 'H5P_DEFAULT');
     
-    C = onCleanup(@() cleanup(dset,space,datatype,file));
+    %C = onCleanup(@() cleanup(dset,space,datatype,file));
     
     device = [char(deviceManufacturer) '.' char(deviceName)];
     
@@ -548,19 +546,18 @@ function readResponse(epoch, source, deviceName, deviceManufacturer, reader, res
         srate,...
         'Hz');
 
-    response = epoch.insertNumericMeasurement(deviceName,...
+    
+    epoch.insertNumericMeasurement(deviceName,...
         arrayToSet({source.getLabel()}),...
         arrayToSet({device}),...
         data);
     
     disp(['      Inserted response ' char(deviceName)]);
     
-    function cleanup(dset,space,datatype,file)
-        H5D.close(dset);
-        H5S.close(space);
-        H5T.close(datatype);
-        H5F.close(file);
-    end
+    H5D.close(dset);
+    H5S.close(space);
+    H5T.close(datatype);
+    H5F.close(file);
     
 end
 
